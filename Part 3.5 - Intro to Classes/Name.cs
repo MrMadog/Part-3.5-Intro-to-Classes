0daee1137a35d7f57cb4399de49531185b0784cb
Maddox Macnamara <tpmac001maddog@gmail.ca>
2023-02-22 17:44:05 -0500

    Add project files.

---
+++ Part 3.5 - Intro to Classes/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3._5___Intro_to_Classes
+{
+    public class Name
+    {
+        private string _title;
+        private string _firstName;
+        private string _lastName;
+
+        public Name(string firstName, string lastName, string title)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = title;
+        }
+        public Name(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = "";
+        }
+        }
+    }
+
---
+++ Part 3.5 - Intro to Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Part_3._5___Intro_to_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //
+
+        }
+    }
+}
